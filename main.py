aa56d3a4d18cfbc66a33123c7eaf3d6df4fb6506
Yulia51188 <sun51188@gmail.com>
2021-03-23 14:23:19 +0300

    Add first requests to store

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+
 def main():
-    pass
+    load_dotenv()
+    client_id = os.getenv("CLIENT_ID")
+    base_url = os.getenv("MOLTIN_API_BASE_URL")
+    data = {
+        'client_id': client_id,
+        'grant_type': 'implicit',
+    }
+    response = requests.post(f'{base_url}/oauth/access_token', data=data)
+    response.raise_for_status()
+    store_access_token = response.json()["access_token"]
+    print(store_access_token)
+
+    headers = {
+        'Authorization': f'Bearer {store_access_token}',
+    }
+    response = requests.get(f'{base_url}/v2/products', headers=headers)
+    response.raise_for_status()
+    print(response.text)
 
 
 if __name__ == '__main__':
