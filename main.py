f55d8bdda785b2d6e95ba78a9fe3e393cd1c4919
Yulia51188 <sun51188@gmail.com>
2021-03-27 17:32:34 +0300

    Do code decomposition

--- main.py
+++ main.py
@@ -4,25 +4,32 @@ import requests
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
-    client_id = os.getenv("CLIENT_ID")
-    base_url = os.getenv("MOLTIN_API_BASE_URL")
+def get_products(url, token):
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(f'{url}/v2/products', headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_access_token(url, client_id):
     data = {
         'client_id': client_id,
         'grant_type': 'implicit',
     }
-    response = requests.post(f'{base_url}/oauth/access_token', data=data)
+    response = requests.post(f'{url}/oauth/access_token', data=data)
     response.raise_for_status()
-    store_access_token = response.json()["access_token"]
-    print(store_access_token)
+    return response.json()["access_token"]
 
-    headers = {
-        'Authorization': f'Bearer {store_access_token}',
-    }
-    response = requests.get(f'{base_url}/v2/products', headers=headers)
-    response.raise_for_status()
-    print(response.text)
+
+def main():
+    load_dotenv()
+    client_id = os.getenv("CLIENT_ID")
+    base_url = os.getenv("MOLTIN_API_BASE_URL")
+    store_access_token = get_access_token(base_url, client_id)
+    products = get_products(base_url, store_access_token)
+    print(products)
 
 
 if __name__ == '__main__':
