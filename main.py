34fe17ac33fa0904c28cb9f507dd5592a3b08d90
Yulia51188 <sun51188@gmail.com>
2021-03-30 17:34:54 +0300

    Debug add to cart function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_products(url, token):
     }
     response = requests.get(f'{url}/v2/products', headers=headers)
     response.raise_for_status()
-    return response.json()
+    return response.json()['data']
 
 
 def get_access_token(url, client_id):
@@ -23,14 +23,35 @@ def get_access_token(url, client_id):
     return response.json()["access_token"]
 
 
+def add_item_to_cart(item_id, quantity, url, token):
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "data": {
+            "id": "157c27e4-82bf-46ba-8947-9c9cc8b28b19",
+            "type": "cart_item",
+            "quantity": 1
+        }
+    }
+    response = requests.post(
+        f'{url}/v2/carts/:reference/items',
+        headers=headers,
+        json=payload
+    )
+    response.raise_for_status()
+    return response.json()["data"]
+
+
 def main():
     load_dotenv()
     client_id = os.getenv("CLIENT_ID")
     base_url = os.getenv("MOLTIN_API_BASE_URL")
     store_access_token = get_access_token(base_url, client_id)
     products = get_products(base_url, store_access_token)
-    print(products)
-
+    add_item_to_cart(products[0]["id"], 2, base_url, store_access_token)
+    
 
 if __name__ == '__main__':
     main()
