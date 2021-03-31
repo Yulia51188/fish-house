313a32ecc0412b8d00d3641e17a99575c3727d35
Yulia51188 <sun51188@gmail.com>
2021-03-31 13:58:42 +0300

    Add buttons to tg bot

--- state_bot_prototype.py
+++ state_bot_prototype.py
@@ -33,7 +33,23 @@ def start(bot, update):
     Бот отвечает пользователю фразой "Привет!" и переводит его в состояние ECHO.
     Теперь в ответ на его команды будет запускаеться хэндлер echo.
     """
-    update.message.reply_text(text='Привет!')
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
+
+                [InlineKeyboardButton("Option 3", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    return "BUTTON"
+
+
+def handle_button_click(bot, update):
+    query = update.callback_query
+
+    bot.edit_message_text(text="Selected option: {}".format(query.data),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
     return "ECHO"
 
 
@@ -71,6 +87,7 @@ def handle_users_reply(bot, update):
         chat_id = update.callback_query.message.chat_id
     else:
         return
+    logger.info(f'Current user_reply is: {user_reply}')
     if user_reply == '/start':
         user_state = 'START'
     else:
@@ -78,7 +95,8 @@ def handle_users_reply(bot, update):
 
     states_functions = {
         'START': start,
-        'ECHO': echo
+        'ECHO': echo,
+        'BUTTON': handle_button_click,
     }
     state_handler = states_functions[user_state]
     # Если вы вдруг не заметите, что python-telegram-bot перехватывает ошибки.
