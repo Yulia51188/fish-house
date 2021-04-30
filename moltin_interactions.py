42dd8fac80df96aa9e10e42708ff004830f2bb0e
Yulia51188 <sun51188@gmail.com>
2021-04-30 11:42:19 +0300

    Remove logger config from global

--- moltin_interactions.py
+++ moltin_interactions.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 
 MOLTIN_URL = 'https://api.moltin.com'
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 logger = logging.getLogger('fish_store')
 
 
